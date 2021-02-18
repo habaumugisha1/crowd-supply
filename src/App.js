5c3da9daee9ff48b6edbfd00b768c095d5c9c93b
habaumugisha1 <habajeunes2@gmail.com>
2021-02-18 18:06:33 +0200

    removing unused codes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, {useState} from 'react';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './css/layout.css';
 
-import Crowd from './Crowd';
 import Crowdall from './Crowdall';
 import Crowddetail from './Crowddetail';
 import Lendnow from './Lendnow';
 import DashboardPannel from './components/Dashboard/DashboardPannel';
 import ProductDetail from './components/Dashboard/ProductDetail';
-import FakeLogin  from "./components/FakeLogin";
 import ProtectedRouter from "./components/ProtectedRouter";
 import { useHistory } from "react-router-dom";
 import AdminPannel from './components/AdminPannel/AdminPannel';
@@ -18,18 +16,8 @@ import SignUp from "./components/Authentication/SignUp";
 import SignIn from './components/Authentication/SignIn';
 
 function App() {
-let history = useHistory()
-  const [isLogedIn, setIsLogedIn] = useState(true);
 
-  const logIn = () => {
-    setIsLogedIn(true)
-    // history.push('/dashboard')
-    console.log(isLogedIn)
-  }
-  const logOut = () => {
-    setIsLogedIn(false)
-    console.log(isLogedIn)
-  }
+ 
   return (
 
     <div>
@@ -51,10 +39,10 @@ let history = useHistory()
           <Route path="/deseases" component={Deseases} />
         
           
-          <ProtectedRouter exact isAuth={isLogedIn} path="/dashboard" component={DashboardPannel} />
-          <ProtectedRouter exact isAuth={isLogedIn} path="/subcategories" component={Subcategories} />
+          <ProtectedRouter exact  path="/dashboard" component={DashboardPannel} />
+          <ProtectedRouter exact  path="/subcategories" component={Subcategories} />
 
-          <ProtectedRouter exact isAuth={isLogedIn} path="/product/:productId" component={ProductDetail} />
+          <ProtectedRouter exact  path="/product/:productId" component={ProductDetail} />
         <Route path="*">
             <div style={{ dispalay:"flex", textAlign:"center", alignItems:"center", marginTop:"20%"}}><h1> 🤗  404 Page Not found </h1></div>
           </Route>
